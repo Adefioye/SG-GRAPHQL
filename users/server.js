5d0997926cea89939e85e84f97db64196ec2cea8
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-07-02 03:24:29 +0100

    Working on users project

---
+++ users/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = require("./schema/schema");
+
+const app = express();
+const PORT = 5000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
